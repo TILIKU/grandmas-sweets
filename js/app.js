ae55be3a03427e3571eb341ea2baccd0754fd67d
TILIKU <n.eriashvili12@gmail.com>
2022-08-09 00:12:13 +0400

    added cart and fixed js i think its finally done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-document.getElementById("cart-info").addEventListener("click", function() {
-  const cart = document.getElementById("cart");
-  cart.classList.toggle("show-cart");
-  console.log(cart);
-});
+window.onload=function(){
+  document.getElementById("cart-info").addEventListener("click", function() {
+    const cart = document.getElementById("cart");
+    cart.classList.toggle("show-cart");
+    console.log(cart);
+  });
+}
